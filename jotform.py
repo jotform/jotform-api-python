f1a2900e83a52e8062fea1fc45a52a3a01e8229b
ceren <ceren@jotform.com>
2013-06-06 11:33:38 +0300

    endpoints updated

--- jotform.py
+++ jotform.py
@@ -50,72 +50,75 @@ class JotformAPIClient:
     def getUser(self):
         return self.fetchUrl('/user')[0]
 
-    def getForms(self):
-        return self.fetchUrl('/user/forms')
-
-    def getUserFolders(self):
-        path = "/user/" + self.username + "/folders"
-        return self.fetchUrl(path)
-
-    def getUserSettings(self):
-        path = "/user/" + self.username + "/settings"
-        return self.fetchUrl(path)
-
-    def getUserSettingsBySettingKey(self, settingKey):
-        path = "/user/" + self.username + "\/settings\/" + settingKey
-        return self.fetchUrl(path)
-
     def getUserUsage(self):
         path = "/user/" + self.username + "/usage"
         return self.fetchUrl(path)
 
+    def getForms(self):
+        return self.fetchUrl('/user/forms')
+
     def getSubmissions(self):
         path = "/user/" + self.username + "/submissions"
-        return self.fetchUrl(path)
-
-    def getFormById(self, formId):
-        return self.fetchUrl('/forms/' + formId)
-
-    def getQuestionById(self, formId,  questionId):
-        path = "/form/" + formId + "/quesiton/" + questionId
-        return self.fetchUrl(path)
-
-    def getSubmissionsByFormId(self, formId):
-        path = "/form/" + formId + "/submissions"
-        return self.fetchUrl(path)
-
-    def getFilesByFormId(self, formId):
-        path = "/form/" + formId + "/files"
-        return self.fetchUrl(path)
-
-    def getQuestionsByFormId(self, formId):
-        path = "/form/" + formId + "/questions"
-        return self.fetchUrl(path)
-
-    def getUserSubmissionById(self, subId):
-        path = "/user/" + self.username + "\/submission\/" + subId
-        return self.fetchUrl(path)
-
-    def getUserHistory(self):
-        path = "/user/" + self.username + "/history"
-        return self.fetchUrl(path)
-
-    def getUserInvoices(self):
-        path = "/user/" + self.username + "/invoices"
-        return self.fetchUrl(path)
-
-    def getUserKeys(self):
-        path = "/user/" + self.username + "/keys"
         return self.fetchUrl(path)
 
     def getUserSubusers(self):
         path = "/user/" + self.username + "/subusers"
         return self.fetchUrl(path)
 
+    def getUserFolders(self):
+        path = "/user/" + self.username + "/folders"
+        return self.fetchUrl(path)
+
     def getUserReports(self):
         path = "/user/" + self.username + "/reports"
+        return self.fetchUrl(path)
+
+    def getUserSettings(self):
+        path = "/user/" + self.username + "/settings"
+        return self.fetchUrl(path)
+
+    def getUserHistory(self):
+        path = "/user/" + self.username + "/history"
+        return self.fetchUrl(path)
+
+    def getFormById(self, formId):
+        return self.fetchUrl('/forms/' + formId)
+
+    def getQuestionsByFormId(self, formId):
+        path = "/form/" + formId + "/questions"
+        return self.fetchUrl(path)
+
+    def getQuestionById(self, formId,  questionId):
+        path = "/form/" + formId + "/quesiton/" + questionId
+        return self.fetchUrl(path)
+
+
+    def getSubmissionsByFormId(self, formId):
+        path = "/form/" + formId + "/submissions"
+        return self.fetchUrl(path)
+
+    # def postSubmissionsByFormId(self, formId):
+    #     path = "/form/" + formId + "/submissions"
+    #     return self.fetchUrl(path)
+
+
+    def getWebhooksByFormID(self, formId):
+        path = "/form/" + formId + "/webhooks"
+        return self.fetchUrl(path)
+
+    # def addWebhookToForm(self, formId):
+    #     path = "/form/" + formId + "/files"
+    #     return self.fetchUrl(path)
+
+    def getUserSubmissionById(self, subId):
+        path = "/user/" + self.username + "\/submission\/" + subId
         return self.fetchUrl(path)
 
     def getUserReportById(self, reportId):
         path = "/user/" + self.username + "\/report\/" + reportId
         return self.fetchUrl(path)
+
+    def getUserFolderById(self, folderId):
+        path = "/user/" + self.username + "/folder/" + self.folderId
+        return self.fetchUrl(path)
+        
